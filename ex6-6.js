2fdcde9017894625cc84428ba0a8cc52cb73a4f6
sakuratanaka <167832973+sakuratanaka@users.noreply.github.com>
2024-10-22 10:18:37 +0900

    Update ex6-6.js

--- ex6-6.js
+++ ex6-6.js
@@ -1,5 +1,7 @@
 var who66='world';
 function sayhello66(){alert('Hello,'+who66+'!');}
+function sayhello66(){alert('太郎が来た!');}
+function sayhello66(){alert('花子が来た!');}
 function taro66(){who66='太郎';}
 function hanako66(){who66='花子';}
                   
