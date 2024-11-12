4dec27db6f7e22ced9290bb5bbbe4fadf073e767
sakuratanaka <167832973+sakuratanaka@users.noreply.github.com>
2024-11-12 22:06:29 +0900

    Create ex6-10.js

---
+++ ex6-10.js
@@ -0,0 +1,6 @@
+function imgchange1() {
+    document.getElementById('logo').src = './image/jousaikyara.jpg';
+}
+function imgchange2() {
+    document.getElementById('logo').src = './image/josai.jpg';
+}
