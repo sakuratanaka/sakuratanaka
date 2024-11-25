4fc0943577a88b83ecd209057e70b622505786b9
sakuratanaka <167832973+sakuratanaka@users.noreply.github.com>
2024-11-25 22:27:42 +0900

    Update ex6-10.js

--- ex6-10.js
+++ ex6-10.js
@@ -1,6 +1,3 @@
-function imgchange1() {
-    document.getElementById('logo').src = './image/koma.jpg';
-}
-function imgchange2() {
-    document.getElementById('logo').src = './image/unnamed.jpg';
-}
+function imgchange1(){document.getElementById('mark').src='./image/naku.png';}
+function imgchange2(){document.getElementById('mark').src='./image/nikoniko.png';}
+
